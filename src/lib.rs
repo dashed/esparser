f4a6704b590d76f5060380b692701a0f91ee7037
Alberto <mail4alberto@gmail.com>
2016-10-26 00:38:11 -0400

    conditional_expression

--- src/lib.rs
+++ src/lib.rs
@@ -614,6 +614,29 @@ fn initializer<I: U8Input>(i: I, params: Option<Parameter>) -> SimpleResult<I, (
     }
 }
 
+// == 12.14 Conditional Operator ( ? : ) ==
+//
+// http://www.ecma-international.org/ecma-262/7.0/#sec-conditional-operator
+
+// TODO: test
+fn conditional_expression<I: U8Input>(i: I, params: Option<Parameter>) -> SimpleResult<I, ()> {
+
+    // validation
+    match params {
+        None |
+        Some(Parameter::In) |
+        Some(Parameter::Yield) => {},
+        _ => {
+            panic!("misuse of conditional_expression");
+        }
+    }
+
+    parse!{i;
+
+        ret {()}
+    }
+}
+
 // == 12.15 Assignment Operators ==
 //
 // http://www.ecma-international.org/ecma-262/7.0/#sec-assignment-operators
