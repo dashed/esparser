e7f7fbac385b048816de6243acadd9224cbf9ba8
Alberto Leal <mailforalberto@gmail.com>
2017-03-03 04:30:06 -0500

    comments

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ mod parsers;
 
 // TODO: implement
 // ECMAScript 2011 Language Specification (5.1 Edition)
+// NOTE: Same as 5th Edition with corrections.
 // pub mod june_2011;
 // pub use june_2011 as ecmascript_2011;
 // pub use june_2011 as es_2011;
@@ -30,3 +31,8 @@ pub mod june_2016;
 pub use june_2016 as ecmascript_2016;
 pub use june_2016 as es_2016;
 pub use june_2016 as es7;
+
+// TODO: implement
+// Next ECMAScript in development (8 Edition)
+// pub mod edge;
+// pub mod edge as es8;
