4d2baf39d03de747519ca8d092c11bfb9b86175f
Alberto Leal <mailforalberto@gmail.com>
2017-03-09 11:38:02 -0500

    iterable_enum

--- src/june_2016/types.rs
+++ src/june_2016/types.rs
@@ -7,6 +7,7 @@ use std::mem;
 use enum_set::{EnumSet, CLike};
 
 // Ref: https://is.gd/kEGYIp
+// TODO: `ag is_debug_mode` should return only 1 result
 macro_rules! ensure_params {
     ($input:expr; $callee:expr; $( $test:expr );*) => {
 
@@ -21,27 +22,46 @@ macro_rules! ensure_params {
     };
 }
 
+// TODO: better name
+macro_rules! iterable_enum {
+    (enum $enum_name:ident { $( $enum_item:ident,)* } ) => (
+
+        #[repr(u32)]
+        #[derive(Clone)]
+        pub enum $enum_name {
+            $($enum_item,)*
+        }
+
+        impl CLike for $enum_name {
+            fn to_u32(&self) -> u32 {
+                let encoded: Self = self.clone();
+                encoded as u32
+            }
+
+            unsafe fn from_u32(v: u32) -> Self {
+                mem::transmute(v)
+            }
+        }
+
+        // Return vector containing all the enum values
+        impl $enum_name {
+            fn make_vec() -> Vec<Self> {
+                vec![$($enum_name::$enum_item,)*]
+            }
+        }
+    )
+}
+
 // Based on: http://www.ecma-international.org/ecma-262/7.0/#sec-grammar-notation
 //
 // Used to conditionally include or exclude production grammar rules.
-#[repr(u32)]
-#[derive(Clone)]
-pub enum Parameter {
-    In,
-    Yield,
-    Return,
-    Default,
-}
-
-impl CLike for Parameter {
-    fn to_u32(&self) -> u32 {
-        let encoded: Self = self.clone();
-        encoded as u32
+iterable_enum!(
+    enum Parameter {
+        In,
+        Yield,
+        Return,
+        Default,
     }
-
-    unsafe fn from_u32(v: u32) -> Self {
-        mem::transmute(v)
-    }
-}
+);
 
 pub type Parameters = EnumSet<Parameter>;
